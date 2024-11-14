e0d1c88b73f7504c420d690540c27c0f26f0a5bf
ricsie_cruz <ricsie_aguila@yahoo.com>
2024-11-14 13:17:55 +0800

    data

--- src/app/derby-sdfa/derby-sdfa.component.ts
+++ src/app/derby-sdfa/derby-sdfa.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MainService } from '../services/main.service';
 import { CommonModule } from '@angular/common';
 
@@ -7,18 +7,38 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   selector: 'app-derby-sdfa',
   templateUrl: './derby-sdfa.component.html',
-  styleUrl: './derby-sdfa.component.scss'
+  styleUrls: ['./derby-sdfa.component.scss']
 })
-export class DerbySdfaComponent {
+export class DerbySdfaComponent implements OnInit {
   mainService = inject(MainService);
-  data: any;
+  headers: string[] = [];
+  data: any[] = [];
+  weekOnes: string[] = []; // Array to hold all `weekOne` values
 
   constructor() {}
 
   ngOnInit(): void {
-    this.mainService.getData().subscribe((data: any) => {
-      console.log('data', data.derbySdfa)
-      this.data = data.derbySdfa;
+    this.mainService.getData().subscribe((response: any) => {
+      if (response.derbySdfa) {
+        this.headers = response.derbySdfa[0].headers;
+        this.data = response.derbySdfa[1].data;
+
+        console.log('data', this.data)
+
+        // Calculate `weekOne` for each week entry and push to `weekOnes`
+        this.data.forEach(item => {
+          if (item.week1) {
+            item.week1.forEach((week: any) => {
+              const weekOneValue = week.totalBirds
+                ? (week.rank / week.totalBirds).toFixed(2)
+                : "0.00";
+              this.weekOnes.push(weekOneValue);
+            });
+          }
+        });
+
+        console.log('weekOnes', this.weekOnes); // Check values in console
+      }
     });
   }
 }
